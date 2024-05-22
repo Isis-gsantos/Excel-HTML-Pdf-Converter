b8dd920f0e7506b253e1d3032e1d90493f473dda
Isis Gonçalves Santos <isissantos8425@gmail.com>
2024-05-22 18:13:45 -0300

    removing comments

--- HTMLParser.js
+++ HTMLParser.js
@@ -2,23 +2,7 @@ const ejs = require("ejs");
 
 class HtmlParser {
     static async Parse(table) {
-        //para converter a tabela para o html precisa de um arquivo html do ejs base para gerar as tabelas, que é o table.ejs
-        /*
-            dentro do body do arquivo ejs primeiramente foi colocado as seguintes tags para depois prosseguir com o que vai dentro desse método Parse (conteúdo abaixo)
-            <body>
-                <table>
-                    <thead>
-                        <!-- quero printar o cabeçalho, que é um array -->
-                        <% header.forEach(col => { %>
-                            <th><%= col %></th>
-                            <!-- para cada elemento dentro do cabeçalho vou fazer algo e esse algo é printar a coluna no html -->
-                        <% }) %>
-                    </thead>
-                </table>
-            </body>
-        */
         return await ejs.renderFile("./table.ejs", {header: table.header, rows: table.rows});
-        //com o renderFile estou dizendo para pegar o arquivo ejs, olhe o que tem dentro dele (conteúdo citado acima) e processe esse arquivo. Dentro dele falo qual arquivo quero processar, como segundo parâmetro passar os dados que o ejs precisa para printar por ex o cabeçalho (variável header)
     }
 }
 
--- PDFWriter.js
+++ PDFWriter.js
@@ -2,10 +2,7 @@ const pdf = require("html-pdf");
 
 class PDFWriter {
     static WritePDF(filename, html) {
-        //passa o nome do arquivo que quero gerar e um html para gerar o arquivo
         pdf.create(html, {}).toFile(filename, (err) => {})
-        //passa o conteúdo que quero inserir dentro do pdf que vai vir no html, posso passar algumas opções (mas não foi feito nesse caso)
-        //além disso vou dizer que quero gerar esse pdf para um arquivo "toFile", dentro dele fala o nome do arquivo que vou receber no primeiro parâmetro (filename) e ela precisa ter um callback(que não foi necessário)
     }
 }
 
